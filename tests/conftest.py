f1b125536c1b2a4403e44175ea5260cb2c581cb2
Eugenia Oshurko <yarutoua@gmail.com>
2021-04-06 14:21:03 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from neo4j import GraphDatabase
 # Neo4j credentials (should be moved to some config files or env vars)
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "admin"
+NEO4J_PASSWORD = "neo4j"
 
 
 @pytest.fixture(scope="module")
